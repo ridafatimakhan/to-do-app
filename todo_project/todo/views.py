b79718a3653fba031a432076892f0fe3b83aaf24
Rida Fatima <92318911+ridafatimakhan@users.noreply.github.com>
2025-01-22 15:47:20 +0200

    backend setup

---
+++ todo_project/todo/views.py
@@ -0,0 +1,27 @@
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'todo/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+        return redirect('task_list')
+    return render(request, 'todo/add_task.html')
+
+def complete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.completed = True
+    task.save()
+    return redirect('task_list')
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('task_list')
